13400689dd3fc29230f6cd767bdcc70d6ba77832
omdev74 <omdev74@gmail.com>
2021-08-17 06:10:16 +0530

    @0.8.10 Adding a Dynamic Ingredient Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React,{Component} from "react";
-
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./components/Layout/Layout";
 class App extends Component{
   render(){
     return(
       <div>
         <Layout>
-          <p>Testing...</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     )
